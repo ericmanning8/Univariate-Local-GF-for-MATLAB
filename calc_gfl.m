b3186a2c81d66722f181d99ad58933e040bbcdbd
Eric Manning <emanning@usc.edu>
2019-09-10 13:41:26 -0700

    Added calc gfl function

---
+++ calc_gfl.m
@@ -0,0 +1,17 @@
+% Estimate GF L(d) assuming NO edge corrections
+
+function  gf_l = calc_gfl(gf_l, x1, y1, t_incr, i, points, area)
+
+    for i = 1:n %Loop through all points
+        for j = 1:n
+            if ((abs(y1(i) - y1(j)) < t_incr) && (abs(x1(i) - x1(j)) < t_incr)) % Skip i=j and end loop when distance between i and j too big (assumed lists ordered)
+
+                if ( (i ~= j) && (points_in_circle(x1(i), y1(i), x1(j), y1(j), t_incr) = true) )
+                    gf_l(i, bin) = gf_l(i, bin) + 1; %Update value of GF l(d)
+                end
+
+            end
+    
+        gf_l(i, bin) = sqrt(area * gf_l(i, bin) / (pi * (n - 1))) %Final GF L value for i at bin
+
+end
