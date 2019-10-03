aa5b08b9bdf7e3af78dc67ff113fe6c46fd24f11
Eric Manning <emanning@usc.edu>
2019-10-03 15:15:56 -0700

    created main script and aded choose edge correction function to files

---
+++ MAIN.m
@@ -0,0 +1,23 @@
+%%% Main Script
+
+% load data 
+data;
+x1 = data(:,1);
+y1 = data(:,2);
+st = 10;
+max_step = 1000;
+points = length(x1);
+
+% choose method of edge correction
+method = ChooseEdgeCorrection();
+
+% perform GFL analysis 
+if (method == 1) % NDF with edge corrections 
+    gfl = calc_gfl_ew(x1, y1, st, max_step, points);
+else % without edge corrections 
+    gfl = calc_gfl(x1, y1, st, max_step, points);
+end
+
+% display the NDF vector 
+disp('GLF vector:');
+disp(gfl(:,1));
